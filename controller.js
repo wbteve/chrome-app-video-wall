f21c33abf47f3dca9c9912c8afa34cef784ad6c6
djn000 <djn000@gmail.com>
2012-11-19 15:02:31 +0000

    DN: Add styling

--- controller.js
+++ controller.js
@@ -1,19 +1,24 @@
 function WallCtrl($scope) {
 
+	console.log('in controller');
+
 	var defaultDropText = "Drop files here";
   	$scope.dropText = defaultDropText;
 
   	var dragOver = function(e) {
+  		console.log('drag over');
 	    e.stopPropagation();
 	    e.preventDefault();
 	    var valid = e.dataTransfer && e.dataTransfer.types && ( e.dataTransfer.types.indexOf('Files') >= 0 || e.dataTransfer.types.indexOf('text/uri-list') >=0 )
 	    $scope.$apply(function() {
 	      	$scope.dropText = valid ? "Drop files and remote images and they will become Todos" : "Can only drop files and remote images here";
+	      	console.log("dragging");
 	      	$scope.dropClass = valid ? "dragging" : "invalid-dragging";
 	    });
   	}
 
   	var dragLeave = function(e) {
+  		console.log('drag leave');
     	$scope.$apply(function() {
       		$scope.dropText = defaultDropText;
       		$scope.dropClass = '';
@@ -21,6 +26,7 @@ function WallCtrl($scope) {
   	}
 
   	var drop = function(e) {
+  		console.log('drop');
     	e.preventDefault();
     	e.stopPropagation();
 
@@ -40,7 +46,7 @@ function WallCtrl($scope) {
       $scope.dropText = defaultDropText;
       $scope.dropClass = '';
       for (var i = 0; i < newFiles.length; i++) {
-        $scope.todos.push(newFiles[i]);
+        $scope.newFiles.push(newFiles[i]);
       }
       $scope.save();
     });
